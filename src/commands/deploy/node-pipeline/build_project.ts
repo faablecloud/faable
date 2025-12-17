29fa3bb1eddbf19ac4ef589f1d8a8ae7f5ac57ff
Marc Pomar <marc@faable.com>
2025-12-17 20:27:21 +0100

    fix: show output

--- src/commands/deploy/node-pipeline/build_project.ts
+++ src/commands/deploy/node-pipeline/build_project.ts
@@ -27,6 +27,7 @@ export const build_project = async (args: BuildProjectArgs) => {
       timeout,
       cwd,
       enableOutput: true,
+      env: args.env,
     });
   } else {
     log.info(`⚡️ No build step`);
