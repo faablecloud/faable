69282a6481406bbf1fa85d4b2c1448ee2f688a4d
Marc Pomar <marc@faable.com>
2026-04-06 15:34:11 +0200

    fix: deploy is only allowed with app_id

--- src/commands/deploy/index.ts
+++ src/commands/deploy/index.ts
@@ -1,23 +1,91 @@
-import { CommandModule } from "yargs";
+import { CommandModule } from 'yargs'
+import { FaableApp } from '../../api/FaableApi'
+import { context } from '../../api/context'
+import { cmd } from '../../lib/cmd'
+import { log } from '../../log'
+import { check_environment } from './check_environment'
+import { build_node } from './node-pipeline'
+import { runtime_detection } from './runtime-detect/runtime_detection'
+import { upload_tag } from './upload_tag'
 
-import { deploy_command, DeployCommandArgs } from "./deploy_command";
+export interface DeployCommandArgs {
+  app_id: string
+  workdir?: string
+  project_type?: string
+}
 
-export const deploy: CommandModule<{}, DeployCommandArgs> = {
-  command: "deploy [app_slug]",
-  describe: "Deploy a faable app",
-  builder: (yargs) => {
+export const deploy: CommandModule<unknown, DeployCommandArgs> = {
+  command: 'deploy [app_id]',
+  describe: 'Deploy a faable app',
+  builder: yargs => {
     return yargs
-      .positional("app_slug", {
-        type: "string",
-        description: "App slug",
+      .positional('app_id', {
+        type: 'string',
+        description: 'App Identifier'
       })
-      .option("workdir", {
-        alias: "w",
-        type: "string",
-        description: "Working directory",
+      .option('workdir', {
+        alias: 'w',
+        type: 'string',
+        description: 'Working directory'
       })
-      .showHelpOnFail(false) as any;
+      .showHelpOnFail(false) as any
   },
 
-  handler: deploy_command,
-};
+  handler: async args => {
+    const workdir = args.workdir || process.cwd()
+
+    const ctx = await context()
+    const { api } = ctx
+
+    // Resolve runtime
+    const { runtime } = await runtime_detection(workdir)
+
+    let app: FaableApp | undefined
+    if (args.app_id) {
+      app = await api.getApp(args.app_id)
+    } else {
+      if (ctx.appId) {
+        app = await api.getApp(ctx.appId)
+      }
+    }
+
+    if (!app) {
+      throw new Error('Missing <app_id>')
+    }
+
+    // Check if we can build docker images
+    await check_environment()
+
+    log.info(
+      `🚀 Deploying "${app.name}" (${app.id}) runtime=${runtime.name}-${runtime.version}`
+    )
+
+    // get environment variables
+    const env_vars = await api.getAppSecrets(app.id)
+
+    let type
+
+    if (runtime.name == 'node') {
+      const node_result = await build_node(app, {
+        workdir,
+        runtime,
+        env_vars
+      })
+      type = node_result.type
+    } else if (runtime.name == 'docker') {
+      type = 'node'
+      await cmd(`docker build -t ${app.id} .`, {
+        enableOutput: true
+      })
+    } else {
+      throw new Error(`No build pipeline for runtime=${runtime.name}`)
+    }
+
+    // Upload to Faable registry
+    const { upload_tagname } = await upload_tag({ app, api })
+
+    // Create a deployment for this image
+    await api.createDeployment({ app_id: app.id, image: upload_tagname, type })
+    log.info(`🌍 Deployment created -> https://${app.url}`)
+  }
+}
--- src/commands/deploy/runtime-detect/runtime_detection.ts
+++ src/commands/deploy/runtime-detect/runtime_detection.ts
@@ -1,23 +1,22 @@
-import { strategy_nodejs } from "./strategies/nodejs";
-import * as R from "ramda";
-import { has_any_of_files } from "./helpers/has_any_of_files";
-import { strategy_python } from "./strategies/python";
-import { StrategyResolution, StrategyFn } from "./RuntimeStrategy";
-import { strategy_docker } from "./strategies/docker";
+import * as R from 'ramda'
+import { StrategyFn, StrategyResolution } from './RuntimeStrategy'
+import { has_any_of_files } from './helpers/has_any_of_files'
+import { strategy_docker } from './strategies/docker'
+import { strategy_nodejs } from './strategies/nodejs'
 
 export const runtime_detection = async (
   workdir: string
 ): Promise<StrategyResolution> => {
-  const has = R.curry(has_any_of_files);
+  const has = R.curry(has_any_of_files)
   const strategy = R.cond<[string], StrategyFn>([
-    [has(["package.json"]), R.always(strategy_nodejs)],
+    [has(['package.json']), R.always(strategy_nodejs)],
     // [has(["requirements.txt"]), R.always(strategy_python)],
-    [has(["Dockerfile"]), R.always(strategy_docker)],
-  ])(workdir);
+    [has(['Dockerfile']), R.always(strategy_docker)]
+  ])(workdir)
 
   if (!strategy) {
-    throw new Error("Cannot detect project type");
+    throw new Error('Cannot detect project type')
   }
 
-  return strategy(workdir);
-};
+  return strategy(workdir)
+}
